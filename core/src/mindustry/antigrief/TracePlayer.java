3110d57d4d8fb1f5cad70151296ebf402f56c8df
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-11-30 16:47:37 +0500

    clear traces on map change

--- core/src/mindustry/antigrief/TracePlayer.java
+++ core/src/mindustry/antigrief/TracePlayer.java
@@ -1,17 +1,34 @@
 package mindustry.antigrief;
 
+import arc.*;
 import arc.func.*;
 import arc.struct.*;
+import arc.util.*;
+import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.net.Administration.*;
 import mindustry.net.Packets.*;
+
+import static mindustry.Vars.*;
 
 public class TracePlayer{
     private final ObjectMap<Player, Cons<TraceInfo>> listeners = new ObjectMap<>();
     private final ObjectMap<Player, TraceInfo> infos = new ObjectMap<>();
 
+    private String lastMap;
+
+    public TracePlayer() {
+        Events.on(EventType.WorldLoadEvent.class, e -> {
+            if (state.map.name().equals(lastMap) && net.active()) return;
+            infos.clear();
+            listeners.clear();
+            lastMap = state.map.name();
+        });
+    }
+
     public void trace(Player player, Cons<TraceInfo> onTrace) {
         Call.adminRequest(player, AdminAction.trace);
+        Log.info("[AntiGrief] Requested trace info for " + player.name);
         if (onTrace != null) {
             if (infos.containsKey(player)) {
                 onTrace.get(infos.get(player));
